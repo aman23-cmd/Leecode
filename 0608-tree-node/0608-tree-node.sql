97f7e9a5eb308745a39941dbb2d7cbb45ddade23
Aman Kaushal <amankau2000@gmail.com>
2026-06-02 14:43:55 +0530

    Time: 482 ms (60.65%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,8 +1,35 @@
 # Write your MySQL query statement below
-select id,
-case 
-when p_id is null then 'Root'
-when id in (select p_id from Tree)then 'Inner'
-else 'Leaf'
-end as type
-from Tree
+WITH RECURSIVE TreeHierarchy AS
+(
+    SELECT
+        id,
+        p_id,
+        1 AS level
+    FROM Tree
+    WHERE p_id IS NULL
+
+    UNION ALL
+
+    SELECT
+        t.id,
+        t.p_id,
+        th.level + 1
+    FROM Tree t
+    JOIN TreeHierarchy th
+        ON t.p_id = th.id
+)
+
+SELECT
+    id,
+    CASE
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN
+        (
+            SELECT DISTINCT p_id
+            FROM Tree
+            WHERE p_id IS NOT NULL
+        ) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM TreeHierarchy
+ORDER BY id;
